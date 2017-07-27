a29fc5130ea8feaab25f6038d2b96d6c88dc6b17
Martijn Vegter <mvegter.coding@gmail.com>
2017-07-27 09:55:37 +0200

    Might Fix Appveyor

--- src/Depressurizer/Helpers/Logger.cs
+++ src/Depressurizer/Helpers/Logger.cs
@@ -94,6 +94,11 @@ namespace Depressurizer.Helpers
         /// <param name="logMessage"></param>
         public void Write(LogLevel logLevel, string logMessage)
         {
+            if (!Directory.Exists(LogPath))
+            {
+                Directory.CreateDirectory(LogPath);
+            }
+
             using (StreamWriter streamWriter = new StreamWriter(ActiveLogFile, true))
             {
                 streamWriter.WriteLine($"{DateTime.Now} {logLevel} | {logMessage}");
