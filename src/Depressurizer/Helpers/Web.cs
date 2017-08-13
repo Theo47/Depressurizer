5422002ff5fe277a67bcd705014ce2b915030910
Martijn Vegter <mvegter.coding@gmail.com>
2017-08-13 16:47:49 +0200

    Small Fixes

--- src/Depressurizer/Helpers/Web.cs
+++ src/Depressurizer/Helpers/Web.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+    This file is part of Depressurizer.
+    Original work Copyright 2017 Martijn Vegter.
+
+    Depressurizer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Depressurizer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Depressurizer.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
@@ -18,7 +36,7 @@ namespace Depressurizer.Helpers
         /// <returns></returns>
         public static Image ImageFromStream(string url)
         {
-            Logger.Instance.Write(LogLevel.Info, $"Loading: {url}");
+            Logger.Instance.Write(LogLevel.Info, $"Loading image: {url}");
 
             Image image = null;
 
