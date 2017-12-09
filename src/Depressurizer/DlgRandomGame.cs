c193445c19dde38121173342e269beb7cce2dfb8
Derek Alexander <tidusiscariot@yahoo.com>
2017-12-09 11:08:08 -0500

    Add the functionality for Random Game Selection.  This functionality will use the currently filtered list of games and select a random game, showing the banner (when applicable) and name of the game.  The concept is to help users with unweildy game libraries that encounter option overload.

---
+++ src/Depressurizer/DlgRandomGame.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Depressurizer
+{
+    public partial class DlgRandomGame : Form
+    {
+        private GameInfo game;
+
+        //Constructor
+        public DlgRandomGame(GameInfo game)
+        {
+            this.game = game;
+
+            InitializeComponent();
+        }
+
+        private void DlgRandomGame_Load(object sender, EventArgs e)
+        {
+            DisplayGame();
+        }
+
+        private void DisplayGame()
+        {
+            //game id's less than zero denote a game external to steam.
+            if (game.Id > 0)
+            {
+                //Check to see if the banner is already downloaded, if not, get it now
+                String bannerFile = String.Format(Properties.Resources.GameBannerPath, Path.GetDirectoryName(Application.ExecutablePath), game.Id);
+                if (!File.Exists(bannerFile))
+                {
+                    Utility.GrabBanner(game.Id);
+                }
+
+                try
+                {
+                    Image gameBannerImage = Image.FromFile(bannerFile);
+                    gameBannerBox.Image = gameBannerImage;
+                }
+                catch (Exception e)
+                {
+                    //process the error
+                    MessageBox.Show(GlobalStrings.RandomGame_Banner_Error, GlobalStrings.Gen_Warning, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    Program.Logger.WriteException($"Failed to load game banner for random game selection: ", e);
+                }
+
+            }
+
+            gameTextBox.Text = game.Name;
+        }
+    }
+}
