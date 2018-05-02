44ac37e467c943aff5931fdfd145ea88fc0b668e
Sebastian Grunow <prodigy@servertube.net>
2018-05-02 23:30:58 +0200

    Undo accidentally changed GNU link.

--- src/Depressurizer/GameDB.cs
+++ src/Depressurizer/GameDB.cs
@@ -14,7 +14,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Depressurizer.  If not, see <https?://www.gnu.org/licenses/>.
+along with Depressurizer.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 using System;
